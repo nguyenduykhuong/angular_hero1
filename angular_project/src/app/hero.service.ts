3ad6f3c2ac6cbc53b168282601a9d4a3eb4ae5be
nguyenduykhuong <nguyen.duy.khuong.co.1.0.2@gmail.com>
2022-08-08 23:54:54 +0700

    commit

--- angular_project/src/app/hero.service.ts
+++ angular_project/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 @Injectable({
@@ -7,7 +8,16 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
   constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
+  getHero(id: number): Observable<Hero>{
+    const hero = HEROES.find(h => h.id === id)!;
+    console.log(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
